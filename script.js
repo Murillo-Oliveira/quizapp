05d84b6fcd8aff8aa9a6e588ac26d3dd3cc27421
Murillo Oliveira <murilloscoliveira@gmail.com>
2024-06-10 03:02:00 +0000

    alteracoes script.js

--- script.js
+++ script.js
@@ -38,3 +38,16 @@ botaoTema.addEventListener("click", () => {
 verificarTema()
 
 verificarTema (body, botaoTema)
+
+verificarTema (body, botaoTema)
+
+const botoesAssunto = document.querySelectorAll(".assuntos button")
+botoesAssunto.forEach(botao => {
+    botao.addEventListener("click", selecionarAssunto)
+})
+
+function selecionarAssunto(evento) {
+    const assunto = evento.target.innerText
+    localStorage.setItem("assunto", assunto)
+    window.location.href = "./pages/quiz/quiz.html"
+}
