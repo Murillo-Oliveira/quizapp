7ecafa6cd2923fd3991b30f1ca77bb11777e1576
Murillo Oliveira <murilloscoliveira@gmail.com>
2024-06-10 03:25:32 +0000

    quase la

--- script.js
+++ script.js
@@ -1,45 +1,13 @@
-let tema = "claro"
-
-const botaoTema = document.querySelector(".tema button")
-botaoTema.addEventListener("click", trocarTema)
 import { trocarTema, verificarTema } from "./helpers/tema-helper.js"
 
 const botaoTema = document.querySelector(".tema button")
 const body = document.querySelector("body")
 
-function trocarTema() {
-    if (localStorage.getItem("tema")) { 
-    tema = localStorage.getItem("tema")
-    }
-
-    if (tema === "claro") {
-        body.classList.add("escuro")
-        localStorage.setItem("tema", "escuro")
-    } else {
-        body.classList.remove("escuro")
-        localStorage.setItem("tema", "claro")
-    }
-}
-
-function verificarTema() {
-    if (localStorage.getItem("tema")) {
-        tema = localStorage.getItem("tema")
-    }
-
-    if (tema === "escuro") {
-        body.classList.add("escuro")
-    }
-}
-
 botaoTema.addEventListener("click", () => {
     trocarTema(body, botaoTema)
 })
 
-verificarTema()
-
-verificarTema (body, botaoTema)
-
-verificarTema (body, botaoTema)
+verificarTema(body, botaoTema)
 
 const botoesAssunto = document.querySelectorAll(".assuntos button")
 botoesAssunto.forEach(botao => {
@@ -47,7 +15,8 @@ botoesAssunto.forEach(botao => {
 })
 
 function selecionarAssunto(evento) {
-    const assunto = evento.target.innerText
+    const classeBotao = evento.target.className
+    const assunto = document.querySelector(`.${classeBotao} span`).innerText
     localStorage.setItem("assunto", assunto)
     window.location.href = "./pages/quiz/quiz.html"
 }
--- tema-helper.js
+++ tema-helper.js
@@ -1,11 +1,11 @@
 let tema = "claro"
 
 export function trocarTema(body, botaoTema) {
-    if (localStorage.getItem("tema")) { 
+    if (localStorage.getItem("tema")) {
         tema = localStorage.getItem("tema")
     }
 
-    if (tema === "claro") {
+    if (tema == "claro") {
         body.classList.add("escuro")
         localStorage.setItem("tema", "escuro")
         botaoTema.style.justifyContent = "flex-end"
